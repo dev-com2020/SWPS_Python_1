e33c34391a4a5d4bdff1cc9ad50071f3d18b9718
devcom <kwp@tlen.pl>
2025-03-22 12:36:11 +0100

    settery i property

---
+++ klasy/Person3.py
@@ -0,0 +1,14 @@
+class Osoba:
+    def __init__(self, name: str, birth_year: int):
+        self.name = name
+        self._birth_year = birth_year
+
+    @property
+    def age(self) -> int:
+        from datetime import date
+        return date.today().year - self._birth_year
+
+    @age.setter
+    def age(self, value: int):
+        from datetime import date
+        self._birth_year = date.today().year - value
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from klasy.Person import Person
 from klasy.Person2 import Persona
+from klasy.Person3 import Osoba
 
 osoba1 = Person("jan", "kowalski")
 osoba2 = Persona("janina", "kowalska")
+osoba3 = Osoba("Tomek", 1983)
 
 print(osoba1.powitanie())
 print(osoba1)
 print(osoba2.powitanie())
 print(osoba2)
+osoba3.age = 25
+print(osoba3.age)
