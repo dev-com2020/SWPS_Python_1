1ec6ffd08dcf1a71d9441b9c30bef11bce531d30
devcom <kwp@tlen.pl>
2025-03-22 14:04:11 +0100

    cwiczenie 1

---
+++ feature/cwiczenia.py
@@ -0,0 +1,9 @@
+idx = "UKI"
+location = "Warsaw Airport"
+max_temp = 16
+min_temp = 1
+preorder = 0.4
+
+# idx : location : max_temp / min_temp / preorder
+
+print(f"{idx:3s} : {location:19s} : {max_temp:2d} / {min_temp:3d} / {preorder:3.2f}")
